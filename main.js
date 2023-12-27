58c35846bf460ca11aa7a37a7100a3859104a672
Cristiano Vieira <127705099+DevCrix@users.noreply.github.com>
2023-12-27 16:19:27 -0300

    v3

--- main.js
+++ main.js
@@ -3,15 +3,9 @@
 const openModal = () => document.getElementById('modal')
     .classList.add('active')
 
-const closeModal = () => document.getElementById('modal')
-    .classList.remove('active')
-
-
-const tempProduto = {
-    nome: "Chuteira",
-    codigo: "Ch001",
-    descricao: "Chuteira de campo",
-    preço: "300"
+const closeModal = () => {
+    clearFields()
+    document.getElementById('modal').classList.remove('active')
 }
 
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_produto')) ?? []
@@ -44,10 +38,16 @@ const createProduto = (produto) => {
 }
 
 const isValidFields = () => {
-   return document.getElementById('form').reportValidity()
+    return document.getElementById('form').reportValidity()
 }
 
 //Interação com o usuario
+
+const clearFields = () => {
+    const fields = document.querySelectorAll('.modal-field')
+    fields.forEach(field => field.value = "")
+}
+
 const saveProduto = () => {
     if (isValidFields()) {
         const produto = {
@@ -56,11 +56,79 @@ const saveProduto = () => {
             descricao: document.getElementById('descricao').value,
             preco: document.getElementById('preco').value
         }
-        createProduto(produto)
-        clearFields()
-        closeModal()
+        const index = document.getElementById('nome').dataset.index
+        if (index == 'new') {
+            createProduto(produto)
+            updateTable()
+            closeModal()
+        } else {
+            updateProduto(index, produto)
+            updateTable()
+            closeModal()
+        }
     }
 }
+
+const createRow = (produto, index) => {
+    const newRow = document.createElement('tr')
+    newRow.innerHTML = `
+    <td>${produto.nome}</td>
+    <td>${produto.codigo}</td>
+    <td>${produto.descricao}</td>
+    <td>${produto.preco}</td>
+    <td>
+    <button type="button" class="button green" id="edit-${index}">Editar</button>
+    <button type="button" class="button red" id="delete-${index}">Excluir</button>
+    </td>
+    `
+    document.querySelector('#tableProduto>tbody').appendChild(newRow)
+}
+
+const clearTable = () => {
+    const rows = document.querySelectorAll('#tableProduto>tbody tr')
+    rows.forEach(row => row.parentNode.removeChild(row))
+}
+
+const updateTable = () => {
+    const dbProduto = readProduto()
+    clearTable()
+    dbProduto.forEach(createRow)
+}
+
+const fillFields = (produto) => {
+    document.getElementById('nome').value = produto.nome
+    document.getElementById('codigo').value = produto.codigo
+    document.getElementById('descricao').value = produto.descricao
+    document.getElementById('preco').value = produto.preco
+    document.getElementById('nome').dataset.index = produto.index
+}
+
+const editProduto = (index) => {
+    const produto = readProduto()[index]
+    produto.index = index
+    fillFields(produto)
+    openModal()
+}
+
+const editDelete = (event) => {
+    if (event.target.type == 'button') {
+
+        const [action, index] = event.target.id.split('-')
+
+        if (action == 'edit') {
+            editProduto(index)
+        } else {
+            const produto = readProduto()[index]
+            const response = confirm(`Deseja excluir o produto ${produto.nome}`)
+            if (response) {
+                deleteProduto(index)
+                updateTable()
+            }
+        }
+    }
+}
+
+updateTable()
 
 //Eventos
 document.getElementById('cadastrarProduto')
@@ -71,3 +139,6 @@ document.getElementById('modalClose')
 
 document.getElementById('salvar')
     .addEventListener('click', saveProduto)
+
+document.querySelector('#tableProduto>tbody')
+    .addEventListener('click', editDelete)
